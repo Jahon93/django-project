ed5f07d2c67d34f761f610f1881b9e9ac9a000a5
Jakhongir Iskandarov <55333953+Jahon93@users.noreply.github.com>
2023-01-25 21:00:12 +0900

    crash-course on Django-rest

---
+++ my_proj/books/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.BookList.as_view(), name='book_list'),
+    path('view/<int:pk>', views.BookView.as_view(), name='book_view'),
+    path('new', views.BookCreate.as_view(), name='book_new')
+]
